db7659deedf249bb260f2f574f24222c812af7a1
xind-carlos <henrique.silva.carlos1103@escola.pr.gov.br>
2025-08-27 12:17:31 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'Geografia',
+    ': O que é a globalização?',
+    'A globalização é o processo de integração econômica, social e cultural entre os países, com o aumento das trocas comerciais e da comunicação.'
+    )
+    
+    criaCartao(
+    'Geografia',
+    ' Quais são as principais fontes de energia renovável no Brasil?',
+    'As principais fontes de energia renovável no Brasil são a energia hidrelétrica, solar, eólica e biomassa.'
+    )
+    
+    criaCartao(
+    'História',
+    ' O que foi a Inconfidência Mineira?',
+    'Foi um movimento separatista ocorrido no Brasil colonial, no final do século XVIII, contra a exploração econômica portuguesa. '
+    )
+    
+    criaCartao(
+    'Atualidades e Sociais',
+    ' O que é o aquecimento global e quais suas principais consequências?',
+    ' O aquecimento global é o aumento da temperatura média da Terra devido à emissão excessiva de gases do efeito estufa. '
+    )
+
